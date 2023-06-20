0d554c859d540f112373d9ab85d895ae41e484d5
David <dq4443@gmail.com>
2023-06-20 20:56:43 +1000

    add secret key to forms

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import *
 
+app.config['SECRET_KEYS'] = 'e8dced98448fa91d15237c9179da49a2'
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
