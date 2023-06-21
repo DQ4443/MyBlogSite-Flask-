364c418c47b172d2192c1aadfb44c84fe68325c9
David <dq4443@gmail.com>
2023-06-21 19:21:22 +1000

    add secret key to main app

--- MyBlog.py
+++ MyBlog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEYS'] = 'e8dced98448fa91d15237c9179da49a2'
 
 posts = [
     {
@@ -25,6 +28,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
