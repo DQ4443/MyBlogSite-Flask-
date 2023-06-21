db334935f9fa0bfe34bedee00b84806f3cd3f158
David <dq4443@gmail.com>
2023-06-21 20:06:24 +1000

    add form functionality to main app

--- MyBlog.py
+++ MyBlog.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, url_for
+import secrets
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
-app.config['SECRET_KEYS'] = 'e8dced98448fa91d15237c9179da49a2'
+app.secret_key = secrets.token_hex(16)
 
 posts = [
     {
@@ -29,9 +29,12 @@ def home():
 def about():
     return render_template('about.html', title="About")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
--- forms.py
+++ forms.py
@@ -1,9 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import *
-
-app.config['SECRET_KEYS'] = 'e8dced98448fa91d15237c9179da49a2'
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
