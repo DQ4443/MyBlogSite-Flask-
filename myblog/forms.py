d02f0cc656b46c90c8213e0be779019a4d9613a9
David <dq4443@gmail.com>
2023-07-19 22:27:13 +1000

    add custom validation checks to catch if username or email already used (present in db)

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,12 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from myblog.models import User
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    # custom validations that check if the username and email already exists inside the db
+
+        # def validate_field(self, field):
+        #     if True:
+        #         raise ValueError('Validatioin Message')
+    
+    def validate_username(self. username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, please choose a different one.')
+
+    def validate_email(self. email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, please use a different email.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
