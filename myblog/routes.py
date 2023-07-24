4b028db913b4242d8b930644608412ed2c84f919
David <dq4443@gmail.com>
2023-07-24 12:23:33 +1000

    add auto fill in current user info

--- myblog/routes.py
+++ myblog/routes.py
@@ -76,6 +76,17 @@ def logout():
 @login_required
 def account():
     form = UpdateAccountForm()
+    # if form submit was valid, update corresponding info in db
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account information has been updated', 'success')
+        return redirect(url_for('account'))
+    # if GET request (when user views without updating), auto fill in current information
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     # image_file variable from static folder
     image_file = url_for('static', filename='flask profile pictures/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
