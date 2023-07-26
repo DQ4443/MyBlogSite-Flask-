6801592831bb491c6693ccf26b850d2d206ed8a2
David <dq4443@gmail.com>
2023-07-26 19:26:36 +1000

    reverse post display order and add user_post route to display all posts of each user when username on home page is clicked

--- myblog/routes.py
+++ myblog/routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=3)
+    # order_by changes post order function; paginate 
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -162,3 +163,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted.', 'success')
     return redirect(url_for('home'))
+
+# route for user page when user clicks on post author link
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    # order_by changes post order function; paginate 
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    return render_template('user_posts.html', posts=posts, user=user)
